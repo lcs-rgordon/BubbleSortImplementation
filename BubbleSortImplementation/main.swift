9166f03541abdf5eebe8131f2ad19135a196fb6d
Russell Gordon <github@russellgordon.ca>
2022-10-20 11:48:23 -0400

    Used a loop to populate the list of numbers.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -11,9 +11,9 @@ import Foundation
 var dataSet: [Int] = []
 
 // Populate the list
-dataSet.append(Int.random(in: 1...100))
-dataSet.append(Int.random(in: 1...100))
-dataSet.append(Int.random(in: 1...100))
+for _ in 1...10 {
+    dataSet.append(Int.random(in: 1...100))
+}
 
 // Print the list
 print(dump(dataSet))
