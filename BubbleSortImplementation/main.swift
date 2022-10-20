6770e8bcd120c08c877937db8db77eb1430044f1
Russell Gordon <github@russellgordon.ca>
2022-10-20 11:42:40 -0400

    Made the list and added three values to it.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+// Create an empty list (array)
+var dataSet: [Int] = []
 
+// Populate the list
+dataSet.append(18)
+dataSet.append(36)
+dataSet.append(9)
+
+// Print the list
+print(dump(dataSet))
