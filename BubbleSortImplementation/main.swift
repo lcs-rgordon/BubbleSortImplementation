933547f3c92b262b4f294248bf9c72146c8feea2
Russell Gordon <github@russellgordon.ca>
2022-10-20 11:44:58 -0400

    Now initializing the list with different numbers each time the program runs.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -11,9 +11,9 @@ import Foundation
 var dataSet: [Int] = []
 
 // Populate the list
-dataSet.append(18)
-dataSet.append(36)
-dataSet.append(9)
+dataSet.append(Int.random(in: 1...100))
+dataSet.append(Int.random(in: 1...100))
+dataSet.append(Int.random(in: 1...100))
 
 // Print the list
 print(dump(dataSet))
