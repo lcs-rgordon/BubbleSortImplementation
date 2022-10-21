f28c1e4c7c0762033e8f3172b21eb0119d8b6557
Russell Gordon <github@russellgordon.ca>
2022-10-21 14:36:23 -0400

    Optimized algorithm: when no swaps occurred on a pass, the sorting algorithm stops (because the array must be in order if there were no swaps).

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -18,9 +18,23 @@ func waitForUserInput() {
 var dataSet: [Int] = []
 
 // Populate the list
-for _ in 1...10 {
-    dataSet.append(Int.random(in: 1...100))
-}
+
+// Best case scenario – already sorted
+dataSet.append(3)
+dataSet.append(6)
+dataSet.append(10)
+dataSet.append(13)
+dataSet.append(17)
+dataSet.append(19)
+dataSet.append(89)
+dataSet.append(93)
+dataSet.append(94)
+dataSet.append(96)
+
+//for _ in 1...10 {
+//    dataSet.append(Int.random(in: 1...100))
+//}
+
 
 // Print the list
 print("Unsorted:")
@@ -30,7 +44,10 @@ waitForUserInput()
 // Loop through the entire array "n" times
 // (however many times there are elements in the array)
 for i in 0..<dataSet.count {
-
+    
+    // Keep track of whether a pair of numbers was swapped
+    var swapped = false
+    
     // One pass through the array to float the highest number to the end
     for j in 0..<dataSet.count - 1 {
         
@@ -44,15 +61,24 @@ for i in 0..<dataSet.count {
             dataSet[j + 1] = temporaryValue // Replace right with the temporary value
             print(" values were swapped.", terminator: "")
             
+            // Note that a swap occured
+            swapped = true
+            
         }
         print("")
         
     }
-
+    
+    
     // Print the array after the pass
     print("Array after pass \(i + 1):")
     print(dataSet)
     waitForUserInput()
     
+    // When no swaps occured, stop the outer loop
+    if swapped == false {
+        break
+    }
+
 }
 
